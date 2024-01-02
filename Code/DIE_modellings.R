6b47787b9acc7d2879128a679a9e56e87dab1b60
Nosa <osaweemmanulnosa@gmail.com>
2024-01-03 00:52:04 +0100

    update

--- Code/DIE_modellings.R
+++ Code/DIE_modellings.R
@@ -2,6 +2,7 @@ library(lme4)
 library(lmerTest)
 library(tidyverse)
 library(MASS)
+library(ggfortify)
 
 digg <- read.csv("C:\\Users\\HP\\Desktop\\DIE MODELLING\\Digg.csv")
 digg <- read_csv("Data/Digg.csv")
@@ -36,7 +37,7 @@ digg %>%
   summarise(mean = mean(Individuals),
             median  = median(Individuals)) 
 
-########################################################################################
+############################################################################################
 
 qqplot(digg$Shannon_H)
 qqnorm(digg$Shannon_H)
@@ -44,28 +45,46 @@ hist(digg$Shannon_H)
 shapiro.test(digg$Shannon_H)
 
 
-
 ind_pred3 <- lmer(Shannon_H ~ Day +  (1|Pitfall),
                     data = digg)
 summary(ind_pred3)
 
-ind_pred4 <- glmer(Individuals ~ Day + (1 | Pitfall),
+
+
+ind_glmm_day <- glmer(Individuals ~ Day + (1 | Pitfall),
                    data = digg, family = poisson(link = "log"))
 
-summary(ind_pred4)
+summary(ind_glmm_day)
 
-ind_pred5 <- glmer(Individuals ~ Day +  (Day | Pitfall),
+ind_pred5 <- glmer(Individuals ~ Day +  (1 | Pitfall) + (1|Period),
                    data = digg, family = poisson(link = "log"))
 
 summary(ind_pred5)
+anova(ind_glmm_day, ind_pred5)
+
+digg$Period <- factor(digg$Period, levels = c("S1", "S2", "S3"))
+
+ind_pred6 <- glmer(Individuals ~ Period +  (1 | Pitfall),
+                   data = digg, family = poisson(link = "log"))
+
+summary(ind_pred6)
+
 
 shannon_pred1 <- lmer(Shannon_H ~ Day +  (1|Pitfall),
                   data = digg)
 summary(shannon_pred1)
 
+shannon_pred2 <- lmer(Shannon_H ~ Period +  (1|Pitfall),
+                      data = digg)
+summary(shannon_pred2)
+
 Dom_pred1 <- lmer(Dominance_D ~ Day +  (1|Pitfall),
                       data = digg)
 summary(Dom_pred1)
+
+Dom_pred2 <- lmer(Dominance_D ~ Period +  (1|Pitfall),
+                  data = digg)
+summary(Dom_pred2)
 
 even_pred1 <- lmer(`Evenness_e^H/S` ~ Day +  (1|Pitfall),
                   data = digg)
@@ -83,13 +102,15 @@ summary(simp_pred3)
 
 
 
-simp_pred4 <- lmer(`Simpson_1-D` ~ Period +   (1|Pitfall),
+simp_pred4 <- lmer(Simpson_1.D ~ Period +   (1|Pitfall),
                    data = digg)
 summary(simp_pred4)
 
-simp_pred5 <- lmer(`Simpson_1-D` ~ Day +   (1|Pitfall),
+simp_pred5 <- lmer(Simpson_1.D ~ Day +   (1|Pitfall) +  (1|Period),
                    data = digg)
 summary(simp_pred5)
+
+anova(simp_pred4, simp_pred5)
 
 AIC(simp_pred3)
 AIC(simp_pred4)
