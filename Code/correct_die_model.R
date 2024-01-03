1ec6f381d715ca07a9e52256e98252159efdc68b
Nosa <osaweemmanuelnosa@gmail.com>
2024-01-03 20:00:27 +0100

    glm

--- Code/correct_die_model.R
+++ Code/correct_die_model.R
@@ -1,5 +1,20 @@
 #####  -------------------indices ~ Day 
 
+library(lme4)
+library(lmerTest)
+library(tidyverse)
+library(MASS)
+library(ggfortify)
+
+digg <- read.csv("C:\\Users\\HP\\Desktop\\DIE MODELLING\\Digg.csv")
+digg <- read_csv("Data/Digg.csv")
+view(digg)
+attach(digg)
+
+fdigg <- digg %>% 
+  filter(`Simpson_1-D` >= 0.0001)
+attach(fdigg)
+
 ind_glmm_day <- glmer(Individuals ~ Day + (1 | Pitfall),
                       data = digg, family = poisson(link = "log"))
 
@@ -20,3 +35,55 @@ summary(simp_glmm_day)
 shan_glmm_day <- glmer(Shannon_H ~ Day +   (1|Pitfall),
                        data = fdigg, family = Gamma(link = "log"))
 summary(shan_glmm_day)
+
+################################################################################
+
+abun_glmm_per <- glmer(Individuals ~ Period +  (1 | Pitfall),
+                   data = digg, family = poisson(link = "log"))
+
+summary(abun_glmm_per)
+
+even_glmm_per <- glmer(`Evenness_e^H/S` ~ Period +   (1|Pitfall),
+                    data = digg, family = Gamma(link = "log"))
+summary(even_glmm_per)
+
+domi_glmm_per <- glmer(Dominance_D ~ Period +   (1|Pitfall),
+                       data = digg, family = Gamma(link = "log"))
+summary(domi_glmm_per)
+
+simp_glmm_per <- glmer(`Simpson_1-D` ~ Period +   (1|Pitfall),
+                       data = fdigg, family = Gamma(link = "log"))
+summary(simp_glmm_per)
+
+shan_glmm_per <- glmer(Shannon_H ~ Period +   (1|Pitfall),
+                       data = fdigg, family = Gamma(link = "log"))
+summary(shan_glmm_per)
+
+##################################################################################  
+
+c_digg <- read.csv("C:\\Users\\user\\Documents\\GitHub\\Projects\\Data\\Digging_in.csv")
+view(c_digg)
+attach(c_digg)
+
+abun_glmm_per <- glm(Individuals ~ Period ,
+                       data = digg, family = poisson(link = "log"))
+
+summary(abun_glmm_per)
+
+even_glmm_per <- glm(`Evenness_e^H/S` ~ Period ,
+                       data = digg, family = Gamma(link = "log"))
+summary(even_glmm_per)
+
+domi_glmm_per <- glm(Dominance_D ~ Period,
+                       data = digg, family = Gamma(link = "log"))
+summary(domi_glmm_per)
+
+simp_glmm_per <- glm(`Simpson_1-D` ~ Period,
+                       data = fdigg, family = Gamma(link = "log"))
+summary(simp_glmm_per)
+
+shan_glmm_per <- glm(Shannon_H ~ Period,
+                       data = fdigg, family = Gamma(link = "log"))
+summary(shan_glmm_per)
+
+
