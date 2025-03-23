6d70024d8d16c8d06bc5bd496d38e689f7ee5d7b
Nosa Osawe <osaweemmanuelnosa@gmail.com>
2025-03-23 19:26:42 +0100

    upp

--- Code/Mixed modelling training.R
+++ Code/Mixed modelling training.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(lme4)
+library(performance)
+library(bbmle)
 
 dat.tf <- read.csv("C:\\Users\\DELL\\Documents\\Git in R\\Projects\\Data\\banta_totalfruits.csv")
 
@@ -36,8 +38,7 @@ ggplot(data = dat.tf, aes(factor(x = gna), y = log(total.fruits + 1))) +
 
 view(dat.tf)
 
-install.packages("performance")
-library(performance)
+
 # Poisson GLMM Given the mean-variance relationship, we
 # will most likely need a model with over-dispersion.  To
 # understand why, let's start with a Poisson model.
@@ -49,3 +50,47 @@ model_performance(mp1) #Shows many performance metric
 hist(resid(mp1), breaks = 50)
 
 check_overdispersion(mp1) 
+
+
+# Negative binomial GLMM using the function glmer.nb()
+mnb1 <- glmer.nb(total.fruits ~ nutrient * amd + rack + status +
+                   (1 | popu) + (1 | gen),
+                 data = dat.tf, 
+                 control = glmerControl(optimizer = "bobyqa"))
+# Control argument specifies the way we optimize the
+# parameter values
+
+check_overdispersion(mnb1)
+
+# Account for over-dispersion
+mpl1 <- glmer(total.fruits ~ nutrient * amd + rack + status +
+                (1 | X) + (1 | popu) + (1 | gen), data = dat.tf, 
+              family = "poisson",
+              control = glmerControl(optimizer = "bobyqa"))
+# popu only
+mpl1.popu <- glmer(total.fruits ~ nutrient * amd + rack + status +
+                     (1 | X) + (1 | popu), data = dat.tf, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa"))
+
+# gen only
+mpl1.gen <- glmer(total.fruits ~ nutrient * amd + rack + status +
+                    (1 | X) + (1 | gen), data = dat.tf, 
+                  family = "poisson", 
+                  control = glmerControl(optimizer = "bobyqa"))
+
+# IC approach using AICc
+ICtab(mpl1, mpl1.popu, mpl1.gen, type = c("AICc"))
+
+library(coefplot2)
+
+# Variance terms
+coefplot2(mpl1, ptype = "vcov", 
+          intercept = TRUE, 
+          main = "Random effect variance")
+
+# Fixed effects
+coefplot2(mpl1, intercept = TRUE,
+          main = "Fixed effect coefficient")
+
+summary(mpl1)
+
