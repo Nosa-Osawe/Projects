e93014214d1bfd0762b3703cca081538bc9b9c9d
Nosa Osawe <osaweemmanuelnosa@gmail.com>
2025-04-08 17:02:55 +0100

    up

--- Code/Multivariates [QCBS].R
+++ Code/Multivariates [QCBS].R
@@ -1,5 +1,6 @@
 require(vegan)
 require(tidyverse)
+require(corrplot)
 
 env <- read.csv("C:\\Users\\DELL\\Documents\\Git in R\\Projects\\Data\\doubsenv.csv", 
                 row.names = 1)
@@ -11,5 +12,18 @@ spe <- spe[-8, ]  # Remove corresponding abiotic data for site 8 (because remove
 
 ab <- table(unlist(spe)) # # Count number of species frequencies in each abundance class
 ab   # 50% of our dataset consists of zeros
+
+# Apply Hellinger transformation to correct for the double zero problem
+spe.hel <- decostand(spe, method = "hellinger")
+
+# check for collinearity in the env data; make corr matrix
+
+corrplot(cor(env), method = c("color"), type = "lower",
+         tl.cex = 0.7, tl.col = "black", addCoef.col='black' )
+
+
+
+# Scale and center env. variables
+env.z <- decostand(env, method = "standardize")
 
 
